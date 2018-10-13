db1c6b70d12aeb772a21f488aa2b4bba93d65631
Ricardo Luis Mender <rlmender@gmail.com>
2018-10-13 00:45:40 -0300

    Can create notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {login: 'sessions/new', logout: 'users/sessions#destroy'}
+  resources :notes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'notes#index'
 end
